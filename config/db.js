b042c05e968466a0627356709157985261810779
shiroe66 <shiroeqky@gmail.com>
2022-05-29 22:59:41 +0500

    feat: then n catch in connect block

--- config/db.js
+++ config/db.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose")
-const link =
-  "mongodb+srv://user:<Aa1234>@cluster0.jqgxgtb.mongodb.net/?retryWrites=true&w=majority"
+require("dotenv").config()
 
 const connectDB = () => {
-  mongoose.connect(link, { useNewUrlParser: true })
+  mongoose
+    .connect(process.env.MONGODB_URL, { useNewUrlParser: true })
+    .then(() => console.log("MongoDB connected"))
+    .catch((err) => console.log(err))
 }
 
 module.exports = connectDB
