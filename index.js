f6cc60713122684af6c13a5da46a59fb8074f862
shiroe66 <shiroeqky@gmail.com>
2022-05-29 22:26:14 +0500

    feat: link routes

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require("express")
 const app = express()
 
 const indexRoutes = require("./routes/index")
+const linkRoutes = require("./routes/links")
 
 app.set("view engine", "ejs")
 app.set("views", "views")
 
 app.use(indexRoutes)
+app.use("/links", linkRoutes)
 
 app.listen(3000)
--- routes/links.js
+++ routes/links.js
@@ -1,7 +1,9 @@
 const express = require("express")
 const router = express.Router()
 
-router.post("/short", (req, res) => {})
+router.post("/short", (req, res) => {
+  const { link } = req.body
+})
 
 router.get("/:code", (req, res) => {})
 
