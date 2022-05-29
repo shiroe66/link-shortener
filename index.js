99c77bbacae9f9d1b8e908d1cca991bde5a25669
shiroe66 <shiroeqky@gmail.com>
2022-05-29 22:10:07 +0500

    feat: render by ejs

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 const express = require("express")
 const app = express()
 
+app.set("view engine", "ejs")
+app.set("views", "views")
+
+app.get("/", (req, res) => {
+  res.render("index")
+})
+
 app.listen(3000)
