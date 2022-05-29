ac791ae6e052ee441c455c877ceba2034c4643ba
shiroe66 <shiroeqky@gmail.com>
2022-05-29 23:14:19 +0500

    feat: body-parser

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express")
+const bodyParser = require("body-parser")
 const app = express()
 const connectDB = require("./config/db")
 
 const indexRoutes = require("./routes/index")
 const linkRoutes = require("./routes/links")
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 app.set("view engine", "ejs")
 app.set("views", "views")
