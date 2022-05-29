08abff0800e1428ae779fdf174ab652d8a6874e6
shiroe66 <shiroeqky@gmail.com>
2022-05-29 23:03:58 +0500

    feat: mongoose schema for links

---
+++ models/link.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose")
+
+const linkSchema = new mongoose.Schema({
+  code: String,
+  source: String,
+  short: String,
+  date: {
+    type: String,
+    default: Date.now,
+  },
+})
+
+module.exports = mongoose.model("link", linkSchema)
