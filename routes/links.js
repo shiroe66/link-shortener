3f22b3d158b6f742507a7e9cb750efbc99c005e8
shiroe66 <shiroeqky@gmail.com>
2022-05-29 23:14:11 +0500

    feat: generate short link

--- routes/links.js
+++ routes/links.js
@@ -1,8 +1,34 @@
 const express = require("express")
+const shortid = require("shortid")
 const router = express.Router()
+const Link = require("../models/link")
 
-router.post("/short", (req, res) => {
+router.post("/short", async (req, res) => {
   const { link } = req.body
+
+  try {
+    const url = await Link.findOne({ link })
+
+    if (url) {
+      return res.json(url)
+    }
+
+    const code = shortid.generate()
+    const shortURL = `localhost:3000/links/${code}`
+
+    url = new Link({
+      code,
+      source: link,
+      short: shortURL,
+    })
+    await url.save()
+    return res.json(url)
+  } catch (e) {
+    return res.status(500).json({
+      status: 500,
+      message: e,
+    })
+  }
 })
 
 router.get("/:code", (req, res) => {})
